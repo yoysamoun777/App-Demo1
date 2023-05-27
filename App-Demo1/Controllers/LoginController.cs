c25bd684545bef3b93c151d3281152311a274fde
yoysamoun777 <126944646+yoysamoun777@users.noreply.github.com>
2023-05-27 10:35:07 +0700

    Add project files.

---
+++ App-Demo1/Controllers/LoginController.cs
@@ -0,0 +1,43 @@
+﻿using App_Demo1.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace App_Demo1.Controllers
+{
+    public class LoginController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult FormLogin()
+        {
+            ClsStudent S;
+            List<ClsStudent> ls = new List<ClsStudent>();
+
+            S = new ClsStudent();
+            
+                S.SID = 1;
+                S.Name = "A";
+                S.Gender = "M";
+            
+            ls.Add(S);
+
+            S = new ClsStudent();
+            
+                S.SID = 2;
+                S.Name = "B";
+                S.Gender = "F";
+            
+            ls.Add(S);
+
+            S = new ClsStudent();
+            S.SID = 3;
+            S.Name = "ABC";
+            S.Gender = "M";
+            ls.Add(S);
+
+            return View(ls);
+        }
+    }
+}
---
+++ App-Demo1/Models/ClsStudent.cs
@@ -0,0 +1,13 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace App_Demo1.Models
+{
+   
+        public class ClsStudent
+        {
+            public int SID { get; set; }
+            public string Name { get; set; }
+            public string Gender { get; set; }
+        }
+    
+}
